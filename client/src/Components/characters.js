b8c5ef87904c631d3024987c2f270db47dbc813b
Mark Chiang <markchiang1@gmail.com>
2018-06-04 14:29:06 -0700

    started Character Component

---
+++ client/src/Components/characters.js
@@ -0,0 +1,12 @@
+import React, {component} from 'react'
+
+class character extends component{
+    state= {
+        list: [],
+        clicked: false,
+        name: '',
+        image: ''
+    }   
+}
+
+export default character
